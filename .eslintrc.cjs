372e7815787185606b4dc387c2beab721617bb81
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-01-10 22:18:25 +0900

    Add eslint-plugin-jsdoc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,17 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: ['eslint:recommended'],
+  plugins: ['jsdoc'],
+  rules: {
+    // jsdoc plugin
+    'jsdoc/check-alignment': 'error',
+    'jsdoc/check-param-names': 'error',
+    'jsdoc/check-property-names': 'error',
+    'jsdoc/check-tag-names': 'error',
+    'jsdoc/check-types': 'error',
+    'jsdoc/check-values': 'error',
+  },
 }
