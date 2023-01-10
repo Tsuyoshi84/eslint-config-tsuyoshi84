ed809ed7e02e8f5f4ea9b78b21ad6256a0bd49cd
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2023-01-10 23:11:04 +0900

    Add eslint-plugin-unicorn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: ['eslint:recommended'],
-  plugins: ['jsdoc'],
+  plugins: ['jsdoc', 'unicorn'],
   rules: {
     // jsdoc plugin
     'jsdoc/check-alignment': 'error',
@@ -13,5 +13,19 @@ module.exports = {
     'jsdoc/check-tag-names': 'error',
     'jsdoc/check-types': 'error',
     'jsdoc/check-values': 'error',
+
+    // unicorn plugin
+    'unicorn/error-message': 'error',
+    'unicorn/explicit-length-check': 'error',
+    'unicorn/number-literal-case': 'error',
+    'unicorn/numeric-separators-style': ['error', { onlyIfContainsSeparator: true }],
+    'unicorn/prefer-array-find': 'error',
+    'unicorn/prefer-array-flat-map': 'error',
+    'unicorn/prefer-array-some': 'error',
+    'unicorn/prefer-includes': 'error',
+    'unicorn/prefer-string-replace-all': 'error',
+    'unicorn/prefer-string-starts-ends-with': 'error',
+    'unicorn/prefer-type-error': 'error',
+    'unicorn/no-useless-spread': 'error',
   },
 }
